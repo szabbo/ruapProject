0d2238562935416345daebec57757a4fdf638b23
szabbo <zeljko.szabbo.sabo@yahoo.com>
2019-08-01 21:00:39 +0200

    some respond errors

---
+++ chocolatePrediction/chocolatePrediction/chocolatePrediction/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace chocolatePrediction
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtCompany = new System.Windows.Forms.TextBox();
+            this.txtSpecificBeanOrigin = new System.Windows.Forms.TextBox();
+            this.txtRef = new System.Windows.Forms.TextBox();
+            this.txtReviewDate = new System.Windows.Forms.TextBox();
+            this.txtCocoaPercent = new System.Windows.Forms.TextBox();
+            this.txtCompanyLocation = new System.Windows.Forms.TextBox();
+            this.txtBeanType = new System.Windows.Forms.TextBox();
+            this.txtBroadBeanOrigin = new System.Windows.Forms.TextBox();
+            this.btn_predict = new System.Windows.Forms.Button();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Company";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Specific Bean Origin";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "REF";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Review Date";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 113);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Cocoa Percent";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 139);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Company Location";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(303, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Rating";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 165);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(59, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Bean Type";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 191);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(93, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Broad Bean Origin";
+            // 
+            // txtCompany
+            // 
+            this.txtCompany.Location = new System.Drawing.Point(197, 6);
+            this.txtCompany.Name = "txtCompany";
+            this.txtCompany.Size = new System.Drawing.Size(100, 20);
+            this.txtCompany.TabIndex = 9;
+            // 
+            // txtSpecificBeanOrigin
+            // 
+            this.txtSpecificBeanOrigin.Location = new System.Drawing.Point(197, 32);
+            this.txtSpecificBeanOrigin.Name = "txtSpecificBeanOrigin";
+            this.txtSpecificBeanOrigin.Size = new System.Drawing.Size(100, 20);
+            this.txtSpecificBeanOrigin.TabIndex = 10;
+            // 
+            // txtRef
+            // 
+            this.txtRef.Location = new System.Drawing.Point(197, 58);
+            this.txtRef.Name = "txtRef";
+            this.txtRef.Size = new System.Drawing.Size(100, 20);
+            this.txtRef.TabIndex = 11;
+            // 
+            // txtReviewDate
+            // 
+            this.txtReviewDate.Location = new System.Drawing.Point(197, 84);
+            this.txtReviewDate.Name = "txtReviewDate";
+            this.txtReviewDate.Size = new System.Drawing.Size(100, 20);
+            this.txtReviewDate.TabIndex = 12;
+            // 
+            // txtCocoaPercent
+            // 
+            this.txtCocoaPercent.Location = new System.Drawing.Point(197, 110);
+            this.txtCocoaPercent.Name = "txtCocoaPercent";
+            this.txtCocoaPercent.Size = new System.Drawing.Size(100, 20);
+            this.txtCocoaPercent.TabIndex = 13;
+            // 
+            // txtCompanyLocation
+            // 
+            this.txtCompanyLocation.Location = new System.Drawing.Point(197, 136);
+            this.txtCompanyLocation.Name = "txtCompanyLocation";
+            this.txtCompanyLocation.Size = new System.Drawing.Size(100, 20);
+            this.txtCompanyLocation.TabIndex = 14;
+            // 
+            // txtBeanType
+            // 
+            this.txtBeanType.Location = new System.Drawing.Point(197, 162);
+            this.txtBeanType.Name = "txtBeanType";
+            this.txtBeanType.Size = new System.Drawing.Size(100, 20);
+            this.txtBeanType.TabIndex = 15;
+            // 
+            // txtBroadBeanOrigin
+            // 
+            this.txtBroadBeanOrigin.Location = new System.Drawing.Point(197, 188);
+            this.txtBroadBeanOrigin.Name = "txtBroadBeanOrigin";
+            this.txtBroadBeanOrigin.Size = new System.Drawing.Size(100, 20);
+            this.txtBroadBeanOrigin.TabIndex = 16;
+            // 
+            // btn_predict
+            // 
+            this.btn_predict.Location = new System.Drawing.Point(222, 224);
+            this.btn_predict.Name = "btn_predict";
+            this.btn_predict.Size = new System.Drawing.Size(75, 23);
+            this.btn_predict.TabIndex = 17;
+            this.btn_predict.Text = "Predict";
+            this.btn_predict.UseVisualStyleBackColor = true;
+            this.btn_predict.Click += new System.EventHandler(this.btn_predict_Click);
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(347, 6);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 259);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.btn_predict);
+            this.Controls.Add(this.txtBroadBeanOrigin);
+            this.Controls.Add(this.txtBeanType);
+            this.Controls.Add(this.txtCompanyLocation);
+            this.Controls.Add(this.txtCocoaPercent);
+            this.Controls.Add(this.txtReviewDate);
+            this.Controls.Add(this.txtRef);
+            this.Controls.Add(this.txtSpecificBeanOrigin);
+            this.Controls.Add(this.txtCompany);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtCompany;
+        private System.Windows.Forms.TextBox txtSpecificBeanOrigin;
+        private System.Windows.Forms.TextBox txtRef;
+        private System.Windows.Forms.TextBox txtReviewDate;
+        private System.Windows.Forms.TextBox txtCocoaPercent;
+        private System.Windows.Forms.TextBox txtCompanyLocation;
+        private System.Windows.Forms.TextBox txtBeanType;
+        private System.Windows.Forms.TextBox txtBroadBeanOrigin;
+        private System.Windows.Forms.Button btn_predict;
+        private System.Windows.Forms.TextBox textBox9;
+    }
+}
+
---
+++ chocolatePrediction/chocolatePrediction/chocolatePrediction/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chocolatePrediction
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //deklaracija svih potrebnih varijabli
+        private string company;
+        private string specificBeanOrigin;
+        private string refVal;
+        private string reviewDate;
+        private string cocoaPercent;
+        private string companyLocation;
+        private string beanType;
+        private string broadBeanOrigin;
+        private string rating = "1";
+
+        public class StringTable
+        {
+            public string[] columnNames { get; set; }
+            public string[,] values { get; set; }
+        }
+
+        private async void btn_predict_Click(object sender, EventArgs e)
+        {
+            //dohvaćanje podataka iz textBoxova
+            company = txtCompany.Text.ToString();
+            specificBeanOrigin = txtSpecificBeanOrigin.Text.ToString();
+            refVal = txtRef.Text.ToString();
+            reviewDate = txtReviewDate.Text.ToString();
+            cocoaPercent = txtCocoaPercent.Text.ToString();
+            companyLocation = txtCompanyLocation.Text.ToString();
+            beanType = txtBeanType.Text.ToString();
+            broadBeanOrigin = txtBroadBeanOrigin.Text.ToString();
+
+
+            //čupanje podataka
+            using (var client = new HttpClient())
+            {
+                var scoreRequest = new
+                {
+
+                    Inputs = new Dictionary<string, StringTable>() {
+                        {
+                            "input1",
+                            new StringTable()
+                            {
+                                columnNames = new string[] { "Company", "Specific Bean Origin", "REF", "Review Date", "Cocoa Percent", "Company Location", "Rating", "Bean Type", "Broad Bean Origin" },
+                                values = new string[,] { { company, specificBeanOrigin, refVal, reviewDate, cocoaPercent, companyLocation, rating, beanType, broadBeanOrigin } }
+
+                            }
+                        },
+                    },
+                    GlobalParameters = new Dictionary<string, string>()
+                    {
+                    }
+                };
+
+                const string apiKey = "/m1nW7l9pUi8kUEui4cYyBH46bOUbESRYApCKecUSp4ES/WTLF7n3G+u5nkRurQMpZ8+r+Dd7/D2CE+r1P7blg=="; // Replace this with the API key for the web service
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+                client.BaseAddress = new Uri("https://ussouthcentral.services.azureml.net/workspaces/444fb7db90244e75a78e014b22cfd457/services/fdd47fb1f38f424d952e8943004b7edb/execute?api-version=2.0&format=swagger");
+
+                // WARNING: The 'await' statement below can result in a deadlock
+                // if you are calling this code from the UI thread of an ASP.Net application.
+                // One way to address this would be to call ConfigureAwait(false)
+                // so that the execution does not attempt to resume on the original context.
+                // For instance, replace code such as:
+                //      result = await DoSomeTask()
+                // with the following:
+                //      result = await DoSomeTask().ConfigureAwait(false)
+
+                HttpResponseMessage response = await client.PostAsJsonAsync("", scoreRequest);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    //hilfe
+                    string result = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine("Result: {0}", result);
+                    MessageBox.Show(result, "Result", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    Console.WriteLine(string.Format("The request failed with status code: {0}", response.StatusCode));
+
+                    // Print the headers - they include the requert ID and the timestamp,
+                    // which are useful for debugging the failure
+                    Console.WriteLine(response.Headers.ToString());
+
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(responseContent);
+                }
+            }
+        }
+    }
+}
